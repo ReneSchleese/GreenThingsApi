7df98ebcd68c4b6054319e6388b10e6b525bfb75
Rene Schleese <reneschleese@gmail.com>
2025-10-04 14:50:45 +0200

    make server serve files and json via endpoint. give bottled-messages.json some sample content

---
+++ server.js
@@ -0,0 +1,18 @@
+﻿import express from "express";
+import fs from "fs";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from /public
+app.use("/files", express.static("public"));
+
+// JSON endpoint for metadata
+app.get("/api/bottled-messages", (req, res) => {
+    const data = JSON.parse(fs.readFileSync("./data/bottled-messages.json", "utf8"));
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
